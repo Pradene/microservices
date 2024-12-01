af5ef21b2b6ed52d67ff46d3fac55bd6d6ad7df8
Liam PRADENE <lpradene@made-f0Ar7s5.clusters.42paris.fr>
2024-12-02 00:54:11 +0100

    Local game bug fix

--- back/game_service/game_service/consumers/game.py
+++ back/game_service/game_service/consumers/game.py
@@ -33,21 +33,21 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
 			await self.close()
 
 		if self.user_id:
-			try:
-				game = await database_sync_to_async(
-					GameModel.objects.get
-				)(id=self.game_id)
-			except GameModel.DoesNotExist:
-				await self.close()
-				return
-
-			if game.status == 'finished':
-				await self.close()
-				return
-
 			self.game_manager = GameManager()
 
 			if self.game_mode == 'remote':
+				try:
+					game = await database_sync_to_async(
+						GameModel.objects.get
+					)(id=self.game_id)
+				except GameModel.DoesNotExist:
+					await self.close()
+					return
+
+				if game.status == 'finished':
+					await self.close()
+					return
+
 				self.game = self.game_manager.get_game(self.game_id)
 				if not self.game:
 					self.game = self.game_manager.create_game(self.game_id)
